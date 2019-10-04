f969892d2299d9be9171b02c70670c3a96aea6e4
simejisan <letam.tamle@gmail.com>
2019-10-04 22:09:06 +0700

    test

--- 4-10.js
+++ 4-10.js
@@ -3,17 +3,18 @@ async function getData() {
     let json = await data.json();
     return json;
 }
-async function main() {
+getData('https://gorest.co.in/public-api/users?_format=json&access-token=cvFY5s2Yb9_Cgx8uudID_KOxQPO99O7NqmRO')
+// async function main() {
 
-    let data = await getData('https://gorest.co.in/public-api/users?_format=json&access-token=cvFY5s2Yb9_Cgx8uudID_KOxQPO99O7NqmRO');
+//     let data = await getData('https://gorest.co.in/public-api/users?_format=json&access-token=cvFY5s2Yb9_Cgx8uudID_KOxQPO99O7NqmRO');
 
-    let tbody = '';
-    data.forEach(item) => {
-        tbody += `<tr>
-    <td>${item.id} </td>
-    <td>${item.first_name + " " + item.last_name}</td>  
-    <td>${item.gender}</td>
-    </tr>`;
-    };
-    document.getElementsByClassName('mytable')[0].tBodies[0].innerHTML = tbody;
-}
+//     let tbody = '';
+//     data.forEach(item) => {
+//         tbody += `<tr>
+//     <td>${item.id} </td>
+//     <td>${item.first_name + " " + item.last_name}</td>  
+//     <td>${item.gender}</td>
+//     </tr>`;
+//     };
+//     document.getElementsByClassName('mytable')[0].tBodies[0].innerHTML = tbody;
+// }
